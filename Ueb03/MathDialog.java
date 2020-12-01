1347f9fd091edbeb7750035cd58acb819d6e362e
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-12-01 18:46:25 +0100

    mouayad inital code

---
+++ Ueb03/MathDialog.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+/**
+ * Dialogklasse zum Testen von MathFunctions klasse
+ *
+ * @author Mouayad Haji Omar, Zubeir Mohamed
+ * @version 1.0
+ */
+public class MathDialog {
+
+}
